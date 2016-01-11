b121bb8c54b2d279ad127bcb465d14ebce25f281
gammarish <gieksiarz@gmail.com>
2016-01-11 18:58:42 +0100

    added login/register template page, route detail, route list

---
+++ LiveHistory/LiveHistory/Controllers/RouteDetailsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LiveHistory.Controllers
+{
+    public class RouteDetailsController : Controller
+    {
+        // GET: RouteDetails
+        public ActionResult RouteDetails()
+        {
+            return View();
+        }
+    }
+}
