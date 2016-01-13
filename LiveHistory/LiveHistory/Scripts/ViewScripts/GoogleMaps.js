47f0d333706b19539871d5e857c52db161e769b4
gammarish <gieksiarz@gmail.com>
2016-01-13 19:40:24 +0100

    geolocalization

--- LiveHistory/LiveHistory/Scripts/ViewScripts/GoogleMaps.js
+++ LiveHistory/LiveHistory/Scripts/ViewScripts/GoogleMaps.js
@@ -49,4 +49,51 @@ function Initialize() {
         });
     }
 
+    var infoWindow = new google.maps.InfoWindow({ map: map });
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Jesteś tutaj');
+            map.setCenter(pos);
+
+            navigator.geolocation.watchPosition(
+            function (position) {
+                setMarkerPosition(
+                    pos,
+                    position
+                );
+            });
+
+            function setMarkerPosition(marker, position) {
+                marker.setPosition(
+                    new google.maps.LatLng(
+                        position.coords.latitude,
+                        position.coords.longitude)
+                );
+            }
+
+        }, function () {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+
+}
+
+
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+                          'Error: The Geolocation service failed.' :
+                          'Error: Your browser doesn\'t support geolocation.');
 }
